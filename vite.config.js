ed2075544730c7468ad4fe467d52443a4cea6b5f
Manuel Galán <manugalangarcia@gmail.com>
2022-10-01 19:07:37 +0200

    chore: Add dependencies and config
    Add remix-router, bootstrap and remix icon packages
    Improve vite config as specified in Bootstrap guide

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
   plugins: [react()]
 })
