48d7093c3f845c8904328bf229de32a5446e8463
Manuel Galán <manugalangarcia@gmail.com>
2022-10-05 22:16:54 +0200

    feat: Add search bar to main view
    Add search bar to the main view and control search bar logic

--- src/views/list/Main.jsx
+++ src/views/list/Main.jsx
@@ -3,28 +3,33 @@ import { useLoaderData } from 'react-router-dom';
 import { select } from '../../utils/dbconnector';
 import Pagination from './components/Pagination';
 import ProductItem from './components/ProductItem';
+import SearchBar from './components/SearchBar';
 
 const ProductList = () => {
 	const ITEMS_PER_PAGE = 8;
-	const [dbObject, tableName] = useLoaderData();
+	const [database, table] = useLoaderData();
 
 	const [page, setPage] = useState(0);
 	const [items, setItems] = useState([]);
+	const [isSearching, setIsSearching] = useState(false)
 
 	useEffect(() => {
-		select(dbObject, tableName, IDBKeyRange.bound(page * ITEMS_PER_PAGE, ((page + 1) * ITEMS_PER_PAGE) - 1)).then((newItems) => {
-			setItems(newItems);
-		});
-	}, [page]);
+		if(!isSearching) {
+			select(database, table, IDBKeyRange.bound(page * ITEMS_PER_PAGE, ((page + 1) * ITEMS_PER_PAGE) - 1)).then((newItems) => {
+				setItems(newItems);
+			});
+		}
+	}, [page, isSearching]);
 
 	return (
 		<div className="container">
+			<SearchBar database={database} table={table} searchSetter={setIsSearching} itemSetter={setItems}></SearchBar>
 			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mt-2 mb-4">
 				{items.map((item) => (
 					<ProductItem key={item.id} itemImg={item.imgUrl} itemName={item.model} itemDescription={item.brand} itemLink={item.id} />
 				))}
 			</div>
-			<Pagination currentPage={page} currentPageSetter={setPage} numberOfPages={12}></Pagination>
+			{!isSearching ? <Pagination currentPage={page} currentPageSetter={setPage} numberOfPages={12}></Pagination> : ''}
 		</div>
 	);
 };
---
+++ src/views/list/components/SearchBar.jsx
@@ -0,0 +1,40 @@
+import { select } from '../../../utils/dbconnector';
+
+const SearchBar = ({ database, table, searchSetter, itemSetter }) => {
+	const search = (event) => {
+		select(database, table, IDBKeyRange.bound(0, Number.MAX_VALUE)).then((allItems) => {
+			const searchTerms = event.target.value.trim() === '' ? [] : event.target.value.trim().split(' ');
+            let filteredItems = allItems;
+
+            if(searchTerms.length) {
+                searchSetter(true)
+            } else {
+                searchSetter(false)
+            }
+
+			for (const searchTerm of searchTerms) {
+                filteredItems = filteredItems.filter((item) => {
+                    let matchesSearchedTerms = false;
+
+                    if (item.model.toLowerCase().match(new RegExp(`(${searchTerm})`)) || item.brand.toLowerCase().match(new RegExp(`(${searchTerm})`)))
+                        matchesSearchedTerms = true;
+
+                    return matchesSearchedTerms;
+                });
+			}
+
+            itemSetter(filteredItems)
+		});
+	};
+
+	return (
+		<div className="input-group mt-4">
+			<span className="input-group-text" id="search-icon">
+                <i class="bi bi-search"></i>
+			</span>
+			<input type="text" className="form-control" placeholder="Search..." aria-label="Search" aria-describedby="search-icon" onInput={search} />
+		</div>
+	);
+};
+
+export default SearchBar;
