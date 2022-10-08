2b868fb84d29b4d0e83127049e7bb055fecb2238
Manuel Galán <manugalangarcia@gmail.com>
2022-10-08 15:18:22 +0200

    style: Improve cards
    Improve cards styling on main view

--- src/views/list/components/ProductItem.jsx
+++ src/views/list/components/ProductItem.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const ProductItem = ({itemImg, itemName, itemDescription, itemLink}) => {
 	return (
         <div className="col">
-            <div className="card">
+            <div className="card h-100">
                 <img src={itemImg} className="card-img-top p-3" alt={itemName} />
-                <div className="card-body">
+                <div className="card-body d-flex flex-column justify-content-end">
                     <h5 className="card-title">{itemName}</h5>
                     <p className="card-text">{itemDescription}</p>
                     <Link to={itemLink} className="btn btn-primary">
-                        Comprar
+                        View more
                     </Link>
                 </div>
             </div>
