bd606ee99b06fc2753032a57d742dbed4a4e1ecc
Manuel Galán <manugalangarcia@gmail.com>
2022-10-08 15:05:39 +0200

    feat: Control data persistence with cookies
    Control when data should be recovered from API using cookies

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.scss'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Cookies from 'universal-cookie';
 
 import Header from './components/Header';
 import ProductList from './views/list/Main';
@@ -15,6 +16,9 @@ import ProductDetails from './views/detail/Main';
 import { openDb, insert } from './utils/dbconnector';
 import { incrementByAmount } from './store/slices/counterSlice';
 import store from './store/main';
+import moment from 'moment/moment';
+
+const cookies = new Cookies()
 
 const routes = createBrowserRouter([
 	{
@@ -25,27 +29,36 @@ const routes = createBrowserRouter([
 			const DB_TABLE = 'products';
 			const TABLE_FIELDS = ['index', 'id', 'brand', 'model', 'price', 'imgUrl'];
 
-			try {
-				const dbObject = await openDb(DB_NAME, [DB_TABLE], [TABLE_FIELDS[0]], [TABLE_FIELDS.slice(1)]);
-
-				const response = await fetch('https://front-test-api.herokuapp.com/api/product');
-				const json = await response.json();
-
+			if(cookies.get(DB_NAME) === undefined) {
+				try {
+					const dbObject = await openDb(DB_NAME, [DB_TABLE], [TABLE_FIELDS[0]], [TABLE_FIELDS.slice(1)]);
+	
+					const response = await fetch('https://front-test-api.herokuapp.com/api/product');
+					const json = await response.json();
+	
 				json.forEach((resObject, index) => {
 					insert(
-						dbObject,
-						DB_TABLE,
-						TABLE_FIELDS,
-						[index, ...TABLE_FIELDS.slice(1).map((field) => resObject[field])]
-					);
-				});
+								dbObject,
+								DB_TABLE,
+								TABLE_FIELDS,
+								[index, ...TABLE_FIELDS.slice(1).map((field) => resObject[field])]
+							);
+					});
 
-				return [dbObject, DB_TABLE];
-			} catch (error) {
-				//If error contains a target, it is an error event; if not, it is the already created database
-				if(!error.target) {
-					return [error, DB_TABLE]
-				} else {
+					cookies.set(DB_NAME, true, {
+						maxAge: moment().add(1, 'hour').diff(moment(), 'seconds')
+					})
+	
+					return [dbObject, DB_TABLE];
+				} catch (error) {
+					console.error('Error while creating and/or interacting with databse. See the error below for more information');
+					console.error(errorEvent);
+				}
+			} else {
+				try{
+					const dbObject = await openDb(DB_NAME, [DB_TABLE], [TABLE_FIELDS[0]], [TABLE_FIELDS.slice(1)]);
+					return [dbObject, DB_TABLE]
+				} catch (error) {
 					console.error('Error while creating and/or interacting with databse. See the error below for more information');
 					console.error(errorEvent);
 				}
@@ -56,10 +69,16 @@ const routes = createBrowserRouter([
 		path: '/:id',
 		element: <ProductDetails />,
 		loader: async({ params }) => {
-			const response = await fetch(`https://front-test-api.herokuapp.com/api/product/${params.id}`);
-			const json = await response.json();
+			if(cookies.get(params.id) === undefined) {
+				const response = await fetch(`https://front-test-api.herokuapp.com/api/product/${params.id}`);
+				const json = await response.json();
+				
+				localStorage.setItem(params.id, JSON.stringify(json))
 
-			localStorage.setItem(params.id, JSON.stringify(json))
+				cookies.set(params.id, true, {
+					maxAge: moment().add(1, 'hour').diff(moment(), 'seconds')
+				})
+			}
 		},
 		action: async({ params, request }) => {
 			let formData = await request.formData();
